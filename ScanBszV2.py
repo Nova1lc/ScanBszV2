e4afcbfd33e3dba220cd62828a23a18851fac187
Nova <141974150+Nova1lc@users.noreply.github.com>
2025-02-09 19:17:26 -0600

    Add files via upload

---
+++ ScanBszV2.py
@@ -0,0 +1,41 @@
+import nmap
+
+banner = """
+
+   _____                 ____        __      _____  
+  / ____|               |  _ \       \ \    / /__ \ 
+ | (___   ___ __ _ _ __ | |_) |___ ___\ \  / /   ) |
+  \___ \ / __/ _` | '_ \|  _ </ __|_  /\ \/ /   / / 
+  ____) | (_| (_| | | | | |_) \__ \/ /  \  /   / /_ 
+ |_____/ \___\__,_|_| |_|____/|___/___|  \/   |____|
+                                                    
+                                                    
+"""
+
+print(banner)
+
+def escanear_vulnerabilidades(ip):
+    scanner = nmap.PortScanner()
+    try:
+        print(f"Escaneando vulnerabilidades en {ip}. . .")
+        resultado = scanner.scan(hosts=ip, arguments='-sS -sCV -sU -A -O --script vuln -T4')
+        
+        for host in resultado['scan']:
+            print(f"\nHost: {host} ({resultado['scan'][host].get('hostaname')})")
+            print(f"Estado: {resultado['scan'][host]['status']['state']}")
+
+            if 'tcp' in resultado['scan'][host]:
+                for puerto, detalles in resultado['scan'][host]['tcp'].items():
+                    print(f"\nPuerto: {puerto}")
+                    print(f"Estado: {detalles['state']}")
+                    if 'script' in detalles:
+                        for script, output in detalles['script'].items():
+                            print(f"- {script}: {output}")
+            else:
+                print("No se encontraron puertos abiertos")
+    except Exception as e:
+        print(f"Error durante el escaneo: {e}")
+ip = input("Ingresa la direccion IP: ")
+escanear_vulnerabilidades(ip)
+
+
